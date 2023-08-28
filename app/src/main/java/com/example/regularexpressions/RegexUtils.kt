1d0ffd9b531c46b057eb8b3204daf9c418e522cd
Andrii Danylovskyi <adanylo@softserveinc.com>
2023-08-28 15:29:40 +0300

    UC#10 Implement regex
    UC#10 Implement regex

---
+++ app/src/main/java/com/example/regularexpressions/RegexUtils.kt
@@ -0,0 +1,23 @@
+package com.example.regularexpressions
+
+class RegexUtils {
+
+    companion object {
+
+        fun checkWithRegex(str: String, maxSize: Int) : Boolean {
+            val regex = Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~])(?=\\S+$).{0,$maxSize}$")
+            /*
+                ^                 # start-of-string
+                (?=.*[0-9])       # a digit must occur at least once
+                (?=.*[a-z])       # a lower case letter must occur at least once
+                (?=.*[A-Z])       # an upper case letter must occur at least once
+                (?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])  # a special character must occur at least once
+                (?=\\S+$)          # no whitespace allowed in the entire string
+                .{0,maxSize}             # anything, not longer then maxSize
+                $                 # end-of-string
+             */
+            return regex.matches(str)
+        }
+
+    }
+}
